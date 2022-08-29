1a145722fb8cea3f94797595f3d6472f744c6d49
DAKingFish <891248330@qq.com>
2022-08-29 16:21:33 +0800

    测试

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
+import App from './App' //根组件
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -10,5 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 )
+/* react18 render() 改了 不能再由ReactDOM创建的,而是ReactDOM的creatRoot()的对象调用 */
 
 reportWebVitals()
